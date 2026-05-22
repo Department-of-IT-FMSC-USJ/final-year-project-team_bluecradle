51de2da62409647c24ab78cc8cd57623d45112d6
Minoli Perera <mnnetmi@gmail.com>
2026-05-22 11:25:55 +0530

    Revert "Feat/core system features"

--- ihms/core_module/views.py
+++ ihms/core_module/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(
-        request,
-        'core_module/home.html',
-        {
-            'title':"BlueCradle - Empowering Sri Lanka's Public Health Excellence"
-        }
-    )
+    return render(request, 'core_module/home.html')
 
 def features(request):
-    return render(
-        request,
-        'core_module/features.html',
-        {
-            'title':'Features - BlueCradle'
-        }
-    )
+    return render(request, 'core_module/features.html')
