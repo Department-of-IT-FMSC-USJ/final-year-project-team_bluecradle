28c6f741ae0583ed8c54ee811234773192959092
MinoliPerera021126 <mnnetmi@gmail.com>
2026-05-22 11:14:51 +0530

    fix: update the code to render page title

--- ihms/core_module/views.py
+++ ihms/core_module/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'core_module/home.html')
+    return render(
+        request,
+        'core_module/home.html',
+        {
+            'title':"BlueCradle - Empowering Sri Lanka's Public Health Excellence"
+        }
+    )
 
 def features(request):
-    return render(request, 'core_module/features.html')
+    return render(
+        request,
+        'core_module/features.html',
+        {
+            'title':'Features - BlueCradle'
+        }
+    )
