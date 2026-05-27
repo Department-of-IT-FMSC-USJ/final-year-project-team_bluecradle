8ce2ad3910aadfb7a9ffcd85e31740d3dc5e4871
MinoliPerera021126 <mnnetmi@gmail.com>
2026-05-27 21:24:53 +0530

    feat(infants): setup infants module for infant management

--- ihms/ihms/urls.py
+++ ihms/ihms/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core_module.urls', namespace='core')),
     path('user/', include('accounts_module.urls', namespace='user')),
+
+    path('infants/', include('infants_module.urls', namespace='infants')),
     path('api/infants/', include('infants_module.urls', namespace='infants')),
 ]
 
