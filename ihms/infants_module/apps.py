f2f8a4eed09df907d642f82b424619f91ba33348
Minoli Perera <mnnetmi@gmail.com>
2026-05-27 20:49:12 +0530

    Delete ihms/infants_module directory
    re-establishing modules

--- ihms/infants_module/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class InfantsModuleConfig(AppConfig):
-    name = 'infants_module'
--- ihms/infants_module/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-app_name = 'infants'
-
-urlpatterns = [
-    path('', views.InfantListCreateView.as_view(), name='infant-list-create'),
-    path('<str:phn>', views.InfantDetailView.as_view(), name='infant-detail')
-]
